b0887fcfeb48b638d380c3e57ef3d4e10e2998d8
Delfina Bravo <bellamente2005@gmail.com>
2023-03-23 22:01:17 -0300

    cambios

--- diadostres/basic.java
+++ diadostres/basic.java
@@ -3,5 +3,6 @@ package diadostres;
 public class basic {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("dia dos y tres");
     }
 }
